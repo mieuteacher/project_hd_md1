93ddcf4d77408c3dd042a2430c105a00ea03bbd6
NTBPhuoc <mieuteacher@gmail.com>
2024-06-29 20:46:37 +0700

    pagein, search

--- usermanager/user-manger.js
+++ usermanager/user-manger.js
@@ -17,8 +17,7 @@ function renderHeader() {
 
 renderHeader()
 
-function renderData(){
-    let userList = JSON.parse(localStorage.getItem("userList"));
+function renderData(userList){
     let htmlStr = ``;
     for(let i = 0; i < userList.length; i++) {
         htmlStr += `
@@ -34,8 +33,6 @@ function renderData(){
     }
     document.querySelector("#user_box").innerHTML = htmlStr;
 }
-renderData()
-
 
 function changeStatusUser(userId) {
     let userList = JSON.parse(localStorage.getItem("userList"));
@@ -60,5 +57,76 @@ function addUser() {
     let userList = JSON.parse(localStorage.getItem("userList"));
     userList.push(newUser)
     localStorage.setItem("userList", JSON.stringify(userList))
-    renderData()
+    changePage(0)
+}
+
+
+// phân trang
+
+let limit = 3; // một trang có 3 user
+let nowPage = 0;
+
+
+function printPageList() {
+    let userList = JSON.parse(localStorage.getItem("userList"));
+    let pageCount = Math.ceil(userList.length / limit);
+
+    let pageBtnList = ``;
+    for(let i = 0; i < pageCount; i++) {
+        pageBtnList += `
+            <button onclick="changePage(${i})" style="color: ${nowPage == i ? "red" : ""}">${i}</button>
+        `
+    }
+    document.querySelector(".page_list").innerHTML = pageBtnList;
+}
+
+// [0,1,2,3,4,5,6]
+
+printPageList()
+
+function loadPageData() {
+
+    let userList = JSON.parse(localStorage.getItem("userList"));
+    // 0 // 1
+    let start = nowPage * limit;  // 0 // 3
+    let end = start + limit; // 3 // 6
+
+    let pageDataList = [];
+    for(let i = start; i < end; i++) {
+        console.log("userList[i]", userList[i])
+        if(userList[i]) {
+            pageDataList.push(userList[i])
+        }else {
+            break
+        }
+        
+
+    }
+
+    renderData(pageDataList)
+}
+
+loadPageData()
+
+function changePage(page) {
+    nowPage = page;
+    printPageList()
+    loadPageData()
+}
+
+function search(event) {
+    let inputSearch = event.target.value;
+    console.log("đã vào", inputSearch)
+    let userList = JSON.parse(localStorage.getItem("userList"));
+
+    let seatchResult = [];
+
+    for(let i in userList) {
+        if(userList[i].userName.includes(inputSearch)) {
+            seatchResult.push(userList[i])
+        }
+    }
+
+
+    renderData(seatchResult)
 }
